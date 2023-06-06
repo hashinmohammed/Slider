e72e4b9b1b2cc8d0bf841d4e8b7a7bccef33a8fb
Hashin Mohammed <hashinmohammed@HASHINs-MacBook-Air.local>
2023-06-06 23:42:17 +0530

    manual slider completed

--- src/components/Courosel.jsx
+++ src/components/Courosel.jsx
@@ -1,35 +1,54 @@
-import React,{useState} from 'react'
-import {shortList,longList,list} from '../data'
-import { FaQuoteRight } from 'react-icons/fa'
-import { FiChevronLeft,FiChevronRight } from 'react-icons/fi'
+import React, { useState } from "react";
+import { shortList, longList, list } from "../data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Courosel = () => {
-    const [people,setPeople]=useState(shortList)
-    const prevSlide=()=>{}
-    const nextSlide=()=>{}
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
+  const prevSlide = () => {
+    setCurrentPerson((oldState) => {
+      const result = (oldState - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentPerson((oldState) => {
+      const result = (oldState + 1) % people.length;
+      return result;
+    });
+  };
 
   return (
- <section className='slider-container'>
-    {people.map((person)=>{
-        const{id,image,name,title,quote}=person
-        return(
-            <article className="slide" key={id}>
-<img src={image} alt={name} className='person-img' />
-<h5 className="name">{name}</h5>
-<p className="title"> {title} </p>
- <p className="text">{quote} </p>
- <FaQuoteRight className='icon' />
-            </article>
-        )
-    })}
-    <button className='prev' onClick={prevSlide}>
+    <section className="slider-container">
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className="slide"
+            key={id}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity:personIndex===currentPerson?1:0,
+              visibility:personIndex===currentPerson?'visible':'hidden',
+            }}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title"> {title} </p>
+            <p className="text">{quote} </p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button className="prev" onClick={prevSlide}>
         <FiChevronLeft />
-    </button>
-    <button className='next' onClick={nextSlide}>
+      </button>
+      <button className="next" onClick={nextSlide}>
         <FiChevronRight />
-    </button>
- </section>
-  )
-}
+      </button>
+    </section>
+  );
+};
 
-export default Courosel
+export default Courosel;
